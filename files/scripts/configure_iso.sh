5e460066efb0f970b23ba9732e4d55a175e34507
Vorxiu <vorxiu@gmail.com>
2026-01-30 07:19:36 +0500

    live user creation

--- files/scripts/configure_iso.sh
+++ files/scripts/configure_iso.sh
@@ -12,3 +12,11 @@ systemctl disable bootloader-update.service
 
 # Install Anaconda for installation
 dnf install -y anaconda-live libblockdev-btrfs
+
+# Create live user account
+useradd -m -G wheel -s /usr/bin/zsh liveuser
+passwd -d liveuser
+chage -d 0 liveuser
+
+# Allow wheel group to use sudo without password
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/wheel-nopasswd
