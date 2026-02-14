f81ab20cc459a42a5ed765b13abbe51394280537
Vorxiu <vorxiu@gmail.com>
2026-02-14 18:50:52 +0500

    some cleanup

--- files/scripts/post_build.sh
+++ files/scripts/post_build.sh
@@ -5,8 +5,6 @@ set -oue pipefail
 mkdir -p /nix
 
 # some code extensions
-ext install ms-vscode-remote.remote-containers
-dnf install qalculate meson libtool cairo-devel rofi-devel
 git clone https://github.com/svenstaro/rofi-calc.git
 cd rofi-calc/
 meson setup build
@@ -18,3 +16,5 @@ dnf remove -y meson libtool cairo-devel rofi-devel
 cd ..
 rm -rf rofi-calc
 
+# install bbrew
+brew install Valkyrie00/homebrew-bbrew/bbrew
