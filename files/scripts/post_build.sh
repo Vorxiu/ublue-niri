a25e78986e0f4dccbf92ae51ee97a72c7f24e7a4
Vorxiu <vorxiu@gmail.com>
2026-03-09 17:13:35 +0500

    test

--- files/scripts/post_build.sh
+++ files/scripts/post_build.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-set -oue pipefail
 # create /nix partition
 mkdir -p /nix
 
-# some code extensions
-git clone https://github.com/svenstaro/rofi-calc.git
-cd rofi-calc/
-meson setup build
-meson compile -C build/
-meson install
-
-# cleanup
-dnf remove -y meson libtool cairo-devel rofi-devel
-cd ..
-rm -rf rofi-calc
-
+echo "test"
 # install bbrew
 # brew install Valkyrie00/homebrew-bbrew/bbrew
